9ce8b2bef7f28691b722942544bec2ac36bbc78b
Deependra <deependra186037@gamil.com>
2024-02-26 17:49:13 +0530

    background color change

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BackgroundColor from './compoment/BackgroundColor'
+
+function App() {
+
+  return (
+    <>
+      <BackgroundColor/>
+    </>
+  )
+}
+
+export default App
